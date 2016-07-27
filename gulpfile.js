ebcf6fcf19c55ad8929139a5c19a9e1d392799d8
David Sosa Valdes <david.sosa.valdes@gmail.com>
2016-07-26 19:29:02 -0500

    Install less and update gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,22 @@
 var gulp = require('gulp');
+var less = require('gulp-less');
+var concat = require('gulp-concat');
+
+// Less Task
+gulp.task('less', function () {
+  return gulp.src('app/less/main.less')
+    .pipe(less({
+      compress: true,
+    }))
+    .pipe(concat('appPokedex.min.css'))
+    .pipe(gulp.dest('app/css'));
+});
 
 // Bower Tasks
-gulp.task('bower-post-install',function(){
-    // Stylesheets
-    gulp.src([
-      'bower_components/font-awesome/css/font-awesome.min.css'
-    ])
-    .pipe(gulp.dest('app/css'));
+gulp.task('bower-post-install', function(){
   	// Less
   	gulp.src('bower_components/bootstrap/less/**').pipe(gulp.dest('app/less/bootstrap'));
+    gulp.src('bower_components/font-awesome/less/**').pipe(gulp.dest('app/less/font-awesome'));
   	// Fonts
   	gulp.src([
   		'bower_components/bootstrap/fonts/**',
